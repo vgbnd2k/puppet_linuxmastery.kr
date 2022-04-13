7eb085c7e887871dbf81ca9d3a57a511f4cd36fb
stinky <stinky@linuxmastery.kr>
2022-04-13 16:23:36 +0900

    Test fortunecow on vm03

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,21 +1,6 @@
 # /home/administrator/code/environments/production/manifests/site.pp
 # 노드 선언
-node 'vm01.linuxmastery.kr' {
-  include base
-  include webapp
-}
-
-node 'vm02.linuxmastery.kr' {
-  include base
-  include webapp
-}
-
 node 'vm03.linuxmastery.kr' {
   include base
-}
-
-node default {
-  notify { 'default_message':
-    message => "==== default ====\n",
-  }
+  include fortunecow
 }
---
+++ modules/fortunecow/manifests/init.pp
@@ -0,0 +1,7 @@
+# /home/administrator/code/environments/production/modules/fortunecow/manifests/init.pp
+# fortunecow 모듈의 기본 매니페스트
+class fortunecow {
+  
+  include fortunecow::install
+
+}
